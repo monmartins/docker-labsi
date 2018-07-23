82028d74be9d66fb6284db9754ef9c4ba3eda02e
Ramon Martins <ramonmartins.campos@gmail.com>
2018-07-23 10:17:59 -0300

    install

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+
+sudo apt-get update -y
+sudo apt-get install docker.io -y
+sudo apt-get install docker-compose -y
+curl -L "https://github.com/docker/machine/releases/download/v0.11.0/docker-machine-$(uname -s)-$(uname -m)" -o /tmp/docker-machine
+chmod +x /tmp/docker-machine
+sudo mv /tmp/docker-machine /usr/local/bin/docker-machine

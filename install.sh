394f5adb593189bd27d891988bc0e6bf405578e4
Ramon Martins <ramonmartins.campos@gmail.com>
2018-07-25 13:01:28 -0300

    add group

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ sudo apt-get install docker-compose -y
 curl -L "https://github.com/docker/machine/releases/download/v0.11.0/docker-machine-$(uname -s)-$(uname -m)" -o /tmp/docker-machine
 chmod +x /tmp/docker-machine
 sudo mv /tmp/docker-machine /usr/local/bin/docker-machine
+sudo groupadd docker
+sudo usermod -aG docker $USER
